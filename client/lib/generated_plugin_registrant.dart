460dbf7b5648c5471d57611ce78df7d64091578e
Alex <ataradachuk@gmail.com>
2021-09-26 21:37:47 -0400

    google sign in works

--- client/lib/generated_plugin_registrant.dart
+++ client/lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -14,5 +15,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
---
+++ client/lib/screens/login_page.dart
@@ -0,0 +1,58 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class LoginPage extends StatelessWidget {
+  Future<FirebaseApp> _initializeFirebase() async {
+        FirebaseApp firebaseApp = await Firebase.initializeApp();
+        return firebaseApp;
+  }
+    Future<UserCredential> signInWithGoogle() async {
+        // Trigger the authentication flow
+        final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+
+        // Obtain the auth details from the request
+        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+        // Create a new credential
+        final credential = GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken,
+            idToken: googleAuth.idToken,
+        );
+
+        // Once signed in, return the UserCredential
+        return await FirebaseAuth.instance.signInWithCredential(credential);
+    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Study Buddy'),
+      ),
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              children: [
+                  ElevatedButton(
+                    child: Text("Google Sign In"),
+                    onPressed: () async{
+                      UserCredential user = await signInWithGoogle();
+                      if (user != null) {
+                        print("USER SIGNED IN");
+                      }
+                    }
+                  ),
+              ],
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
