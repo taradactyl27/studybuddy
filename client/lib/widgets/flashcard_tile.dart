8791a523e8a4b183231287d7e62abaaad4c79a3a
Alex <ataradachuk@gmail.com>
2021-12-12 15:21:58 -0500

    flashcard display on course page

---
+++ client/lib/widgets/flashcard_tile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FlashCardTile extends StatelessWidget {
+  const FlashCardTile({Key? key, required this.name}) : super(key: key);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 5, right: 5),
+      child: Material(
+        elevation: 5,
+        child: SizedBox(
+          height: 150,
+          width: 150,
+          child: Column(
+            verticalDirection: VerticalDirection.up,
+            children: [
+              Container(
+                height: 10,
+                decoration: const BoxDecoration(
+                    gradient:
+                        LinearGradient(colors: [Colors.blue, Colors.blueGrey])),
+              ),
+              Center(
+                child: Container(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(name,
+                      overflow: TextOverflow.ellipsis,
+                      style: GoogleFonts.nunito(
+                        textStyle:
+                            const TextStyle(color: Colors.black, fontSize: 16),
+                      )),
+                ),
+              ),
+              const Icon(Icons.my_library_books, size: 54)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
